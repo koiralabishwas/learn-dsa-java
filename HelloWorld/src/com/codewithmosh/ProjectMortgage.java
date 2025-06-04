f3d43f78f2fa762127d9cd02c037e3b06591ac52
Bishwas Koirala <wasubisu69@gmail.com>
2025-06-04 23:29:23 +0900

    next => control flow

---
+++ HelloWorld/src/com/codewithmosh/ProjectMortgage.java
@@ -0,0 +1,24 @@
+package com.codewithmosh;
+
+import java.util.Scanner;
+
+public class ProjectMortgage {
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+
+    
+    System.out.println("Principal Amoutn : ");
+    long principal = scanner.nextLong();
+    
+    System.out.println("Anual Interest Rate : ");
+    double interestRate = scanner.nextDouble() / 100 / 12;
+
+    System.out.println("Period (Years) :");
+    int years = scanner.nextByte() * 12;
+
+    double mortgage = principal * (Math.pow((1 + interestRate), years)   / (Math.pow(1 + interestRate, years) - 1));
+    System.out.println("mortgage : " + mortgage);
+    scanner.close();
+    
+  }
+}
