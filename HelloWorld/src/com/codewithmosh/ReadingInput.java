ca693d218db5a5add758bb7bbb7ab2f5d906150d
Bishwas Koirala <wasubisu69@gmail.com>
2025-06-04 23:05:44 +0900

    did : scanner

---
+++ HelloWorld/src/com/codewithmosh/ReadingInput.java
@@ -0,0 +1,16 @@
+package com.codewithmosh;
+
+import java.util.Scanner;
+
+public class ReadingInput {
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in); // to work smth in terminal
+    System.out.print("Age : ");
+    byte age = scanner.nextByte();
+    System.out.println("your age is : " + age);
+
+    System.out.print("Name ; ");
+    String name = scanner.nextLine().trim();
+    System.out.println("your name is" + name);
+  }
+}
