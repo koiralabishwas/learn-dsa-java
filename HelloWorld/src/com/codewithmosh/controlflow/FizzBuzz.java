da99bdd2d9243d1b80bb6a7f38e04becf3b67331
Bishwas Koirala <wasubisu69@gmail.com>
2025-06-05 13:54:31 +0900

    did fizz buzz

---
+++ HelloWorld/src/com/codewithmosh/controlflow/FizzBuzz.java
@@ -0,0 +1,40 @@
+package com.codewithmosh.controlflow;
+
+import java.util.Scanner;
+
+public class FizzBuzz {
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    System.out.print("Input : ");
+    int input = scanner.nextInt();
+    System.out.println(fizzByzz(input));
+
+  }
+
+  public static boolean divisibleBy5(int i) {
+    return i % 5 == 0;
+  }
+
+  public static boolean divisibleBy3(int i) {
+    return i % 3 == 0;
+  }
+
+  public static String fizzByzz(int i) {
+    boolean threeDivisible = divisibleBy3(i);
+    boolean fiveDivisible = divisibleBy5(i);
+
+    if (threeDivisible && fiveDivisible) {
+      return "FizzBuzz";
+    }
+
+    if (fiveDivisible) {
+      return "Fizz";
+    }
+
+    if (threeDivisible) {
+      return "Buzz";
+    }
+
+    return String.valueOf(i);
+  }
+}
